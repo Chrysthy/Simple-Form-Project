f0039f1dae7b27f7e90ce320a932bdeb4898029f
Chrystine Martins <chrystine.martins@outlook.com>
2024-06-30 20:26:06 -0300

    update content

--- src/index.js
+++ src/index.js
@@ -7,27 +7,60 @@ const passwordConfirmation = document.getElementById('password-confirmation');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-
+    checkInputUsername();
+    checkInputEmail();
+    checkInputPassword();
 
 });
 
 
 function checkInputUsername() {
-
     const usernameValue = username.value.trim();
 
     if (usernameValue === '') {
-errorInput(username, '')
+        errorInput(username, 'Required');
+
+    } else {
+        const formItem = username.parentElement;
+        formItem.className = 'form-content'
     }
 };
+
+
+function checkInputEmail() {
+    const emailValue = email.value.trim();
+
+    if (emailValue === '') {
+        errorInput(email, 'Required');
+
+    } else {
+        const formItem = email.parentElement;
+        formItem.className = 'form-content'
+    }
+}
+
+function checkInputPassword() {
+    const passwordValue = password.value.trim();
+
+    if (passwordValue === '') {
+        errorInput(password, 'Required');
+
+    } else if (passwordValue.length < 8) {
+        errorInput(password, 'Must contain at least 8 characters')
+
+    } else {
+        const formItem = password.parentElement;
+        formItem.className = 'form-content'
+    }
+}
 
 
 function errorInput(input, message) {
 
     const formItem = input.parentElement;
-    const textMessage = message.querySelector('a');
+    const textMessage = formItem.querySelector('a');
 
-    textMessage.innerText = message
-    formItem.className = 'form-content error'
+    textMessage.innerText = message;
+    formItem.className = 'form-content error';
 
 };
